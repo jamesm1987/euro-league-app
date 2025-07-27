75fa218b914e8108efb8eed91f4d93285d16797c
James McCormack <james@wirebox.co.uk>
2025-07-27 23:14:15 +0100

    migrations

--- database/seeders/CompetitionSeeder.php
+++ database/seeders/CompetitionSeeder.php
@@ -14,28 +14,27 @@ class CompetitionSeeder extends Seeder
     public function run(): void
     {
         $competitions = [
-            ['Premier League', 'league', 'england', null, 39],
-            ['La Liga', 'league', 'spain', null, 140],
-            ['Bundesliga', 'league', 'germany', null, 78],
-            ['Ligue 1', 'league', 'france', null, 61],
-            ['Serie A', 'league', 'italy', null, 135],
-            ['FA Cup', 'cup', 'england', null, 45],
-            ['DFB Pokal', 'cup', 'germany', null, 81],
-            ['Copa del Rey', 'cup', 'spain', null, 143],
-            ['Coupe de France', 'cup', 'france', null, 66],
-            ['Coppa Italia', 'cup', 'italy', null, 137],
-            ['Champions League', 'cup', 'world', null, 2],
-            ['Europa League', 'cup', 'world', null, 3],
-            ['Europa Conference League', 'cup', 'world', null, 848],
+            ['Premier League', 'league', 'england', 39],
+            ['La Liga', 'league', 'spain', 140],
+            ['Bundesliga', 'league', 'germany', 78],
+            ['Ligue 1', 'league', 'france', 61],
+            ['Serie A', 'league', 'italy', 135],
+            ['FA Cup', 'cup', 'england', 45],
+            ['DFB Pokal', 'cup', 'germany', 81],
+            ['Copa del Rey', 'cup', 'spain', 143],
+            ['Coupe de France', 'cup', 'france', 66],
+            ['Coppa Italia', 'cup', 'italy', 137],
+            ['Champions League', 'cup', 'world', 2],
+            ['Europa League', 'cup', 'world', 3],
+            ['Europa Conference League', 'cup', 'world', 848],
         ];
 
-        foreach ($competitions as [$name, $type, $country, $team_id, $api_id]) {
+        foreach ($competitions as [$name, $type, $country, $api_id]) {
             Competition::updateOrCreate(
                 ['api_id' => $api_id], // Unique constraint
             [
                 'name' => $name,
                 'type' => $type,
-                'team_id' => $team_id,
                 'country' => $country,
             ]
         );
